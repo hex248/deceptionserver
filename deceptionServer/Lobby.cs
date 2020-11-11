cb627f3901e54a76bb026457991e3fe46e40cd1f
hex248 <oliverbryan248@gmail.com>
2020-11-11 21:05:59 +0000

    Added lobby class

---
+++ deceptionServer/Lobby.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+
+namespace deceptionServer
+{
+    class Lobby
+    {
+        public string id;
+        public List<Player> players = new List<Player>();
+        public int maxPlayers = 5;
+
+        public Lobby(int _maxPlayers)
+        {
+            this.id = Program.RandomString(5);
+            this.maxPlayers = _maxPlayers;
+        }
+    }
+}
