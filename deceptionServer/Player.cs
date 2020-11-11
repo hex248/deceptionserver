943c4041ee210e4d3b96f0c9f8db2af8471f9cc8
hex248 <oliverbryan248@gmail.com>
2020-11-11 21:06:24 +0000

    Added player class

---
+++ deceptionServer/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace deceptionServer
+{
+    class Player
+    {
+        public IPEndPoint ip;
+        public string username;
+        public string currentLobby;
+        public PhysicalAddress mac;
+
+        public Player(IPEndPoint _ip)
+        {
+            this.ip = _ip;
+        }
+    }
+}
